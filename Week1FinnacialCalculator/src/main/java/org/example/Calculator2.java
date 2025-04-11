1936ae3f168495504d8da1066e5bcba4cd374391
TyreseMontgomery <artsofexpressions@gmail.com>
2025-04-11 15:16:42 -0400

    Final Final cal1

--- Week1FinnacialCalculator/src/main/java/org/example/Calculator2.java
+++ Week1FinnacialCalculator/src/main/java/org/example/Calculator2.java
@@ -2,20 +2,21 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class Calculator2 {
     public static void main(String[] args) {
         // ask the user questions first
         // collect all the data
         // assume information makes sense
         // calculate equation
+        // display number;
+        Scanner scanner = new Scanner(System.in);;
         // display number:
         // |principle = p|
         // |annual Interest rate = r|
         // |loan term = y|
         // |monthly interest rate =i|
         // |Number of monthly payments = n|
-        // |Monthly payments = m |
-        Scanner scanner = new Scanner(System.in);
+        // Monthly payments = m |
         System.out.println("Input principle value of home.");
         double p = scanner.nextDouble();
 
@@ -28,7 +29,7 @@ public class Main {
         double y = scanner.nextDouble();
 
         int n = (int) (y*12);
-         // M = P[r(1+r)^n] / [(1+r)^n-1]
+
         double v = 1 + r;
         double b = Math.pow(v,n);
         double l = b * r;
@@ -37,9 +38,9 @@ public class Main {
         double m = left / right;
         double totalInterest = m * n - p;
 
-        // A $53,000 loan at 7.625% interest for 15 years would
-        //have a $495.09/mo payment with a total interest of $36,115.99
-        System.out.printf("you would have $%.2f payment with a total interest of $%.2f", m, totalInterest);
+
+        System.out.printf("You will pay %.2f$ in total interest over the term .",totalInterest);
+
 
 
     }
