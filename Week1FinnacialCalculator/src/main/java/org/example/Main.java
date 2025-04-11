ea67f849453569b408f90e4a3bc6ffe8d3cb630e
TyreseMontgomery <artsofexpressions@gmail.com>
2025-04-11 11:48:27 -0400

    done at 11:45

--- Week1FinnacialCalculator/src/main/java/org/example/Main.java
+++ Week1FinnacialCalculator/src/main/java/org/example/Main.java
@@ -8,7 +8,39 @@ public class Main {
         // collect all the data
         // assume information makes sense
         // calculate equation
-        // display number;
-        Scanner scanner = new Scanner(System.in);;
+        // display number:
+        // |principle = p|
+        // |annual Interest rate = r|
+        // |loan term = y|
+        // |monthly interest rate =i|
+        // |Number of monthly payments = n|
+        // Monthly payments = m |
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input principle value of home.");
+        double p = scanner.nextDouble();
+
+        System.out.println(" Next, input your interest rate");
+
+        double inputr = scanner.nextDouble();
+        double r = (inputr / 12) /100;
+
+        System.out.println(" Lastly, Input your loan term (in years)");
+        double y = scanner.nextDouble();
+
+        int n = (int) (y*12);
+
+        double v = 1 + r;
+        double b = Math.pow(v,n);
+        double l = b * r;
+        double left = p * l;
+        double right = b - 1;
+        double m = left / right;
+        double totalInterest = m * n - p;
+
+
+        System.out.printf("You will pay %.2f$ in total interest over the term .",totalInterest);
+
+
+
     }
 }
